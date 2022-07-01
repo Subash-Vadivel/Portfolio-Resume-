994bd06541564595aa3b746a2aff00bacdc71ca0
Subash <vsubashvsubash@gmail.com>
2022-07-01 17:17:37 +0530

    first comi

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -21,14 +21,14 @@ from django.conf.urls.static import static
 
 
 #for hosting
-from django.views.static import serve
-from django.conf.urls import url
+#from django.views.static import serve
+#from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', views.index),
     path('blog/', views.blog),
     path('feed/',views.feed),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+#   url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+#   url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
