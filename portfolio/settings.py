8bdeffb1af94ac773ddde5b8585bb64d547fe6b6
Subash <88760720+Subash-Vadivel@users.noreply.github.com>
2022-08-11 15:25:36 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -116,15 +116,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = '/static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'/static'/)]
+STATIC_ROOT =os.path.join(BASE_DIR,'staticfiles')
+STATIC_URL = 'static/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-MEDIA_URL='/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR,'/media/')
+MEDIA_URL='media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
